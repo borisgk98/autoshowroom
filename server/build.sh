2c715e7ce8e12a0f2d0e67c343a17d791a7d9ed6
borisgk98 <borisgk98@ya.ru>
2019-05-21 20:33:51 +0300

    add docker support

---
+++ server/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+docker build . -t merann/autoshowroom:latest
--- server/src/main/java/com/mera/borisgk98/autoshowroom/server/rest/api/OrderApi.java
+++ server/src/main/java/com/mera/borisgk98/autoshowroom/server/rest/api/OrderApi.java
@@ -26,7 +26,7 @@ import javax.validation.constraints.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-05-21T15:49:56.600+03:00[Europe/Moscow]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-05-21T18:27:24.365+03:00[Europe/Moscow]")
 
 @Validated
 @Api(value = "order", description = "the order API")

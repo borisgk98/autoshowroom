97a1d7b0a5df18a27d4fdcc858805748ba0317c3
borisgk98 <borisgk98@ya.ru>
2019-05-28 01:58:10 +0300

    fix order creating

--- server/src/main/java/com/mera/borisgk98/autoshowroom/server/models/Order.java
+++ server/src/main/java/com/mera/borisgk98/autoshowroom/server/models/Order.java
@@ -20,6 +20,7 @@ import javax.validation.constraints.*;
 @ApiModel(description = "Contains information about")
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-05-16T17:07:29.290+03:00[Europe/Moscow]")
 @Entity
+@Table(name = "order_table")
 public class Order implements HasId<Integer> {
   @JsonProperty("id")
   @Id

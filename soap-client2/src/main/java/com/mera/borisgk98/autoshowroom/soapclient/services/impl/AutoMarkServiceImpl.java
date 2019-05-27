371cc4f1c71ab4caba22d60fafdba51948fed280
borisgk98 <borisgk98@ya.ru>
2019-05-27 15:06:35 +0300

    add automark soap impl

--- soap-client2/src/main/java/com/mera/borisgk98/autoshowroom/soapclient/services/impl/AutoMarkServiceImpl.java
+++ soap-client2/src/main/java/com/mera/borisgk98/autoshowroom/soapclient/services/impl/AutoMarkServiceImpl.java
@@ -1,46 +1,51 @@
-package com.mera.borisgk98.autoshowroom.soapclient.services.impldefault;
+package com.mera.borisgk98.autoshowroom.soapclient.services.impl;
 
 import com.mera.borisgk98.autoshowroom.server.soap.*;
-import com.mera.borisgk98.autoshowroom.soapclient.objectfabric.Fabric;
 import com.mera.borisgk98.autoshowroom.soapclient.services.AutoMarkService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.xml.namespace.QName;
 import java.util.List;
 
 @Component
-public class AutoMarkDefaultImpl implements AutoMarkService {
+public class AutoMarkServiceImpl implements AutoMarkService {
 
-    @Autowired
-    protected Fabric fabric;
+    AutoMarkWebService port;
+
+    {
+        AutomarkService ss = new AutomarkService(AutomarkService.WSDL_LOCATION,
+                new QName("http://soap.server.autoshowroom.borisgk98.mera.com/", "automarkService")
+        );
+        port = ss.getAutomarkServicePort();
+    }
 
     @Override
     public AutoMark create(AutoMark m) {
-        return fabric.getObject(AutoMark.class);
+        return port.createAutoMark(m);
     }
 
     @Override
     public AutoMark read(Integer id) throws ModelNotFound_Exception {
-        return fabric.getObject(AutoMark.class);
+        return port.readAutoMark(id);
     }
 
     @Override
     public AutoMark update(AutoMark m) throws ModelNotFound_Exception {
-        return fabric.getObject(AutoMark.class);
+        return port.updateAutoMark(m);
     }
 
     @Override
     public void delete(Integer id) throws ModelNotFound_Exception {
-
+        port.deleteAutoMark(id);
     }
 
     @Override
     public List<AutoMark> getAll() {
-        return fabric.getList(AutoMark.class);
+        return port.getAllAutoMark();
     }
 
     @Override
     public List<AutoMark> getRange(Integer offset, Integer limit) {
-        return fabric.getList(AutoMark.class);
+        return port.getRangeAutoMark(offset, limit);
     }
 }

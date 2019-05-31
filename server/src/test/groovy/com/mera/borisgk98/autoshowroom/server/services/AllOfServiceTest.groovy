8a869eb68379800eb480bd85614488da401da28a
borisgk98 <borisgk98@ya.ru>
2019-05-31 15:13:00 +0300

    Succesful generate tests for all services without OrderService

---
+++ server/src/test/groovy/com/mera/borisgk98/autoshowroom/server/services/AllOfServiceTest.groovy
@@ -0,0 +1,17 @@
+package com.mera.borisgk98.autoshowroom.server.services
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@Suite.SuiteClasses([
+    AutoMarkServiceTest.class,
+    AutoModelServiceTest.class,
+    AutoServiceTest.class,
+    AutoOptionServiceTest.class,
+    SellerServiceTest.class,
+    OrderServiceTest.class,
+    CustomerServiceTest.class
+])
+@RunWith(Suite.class)
+class AllOfServiceTest {
+}

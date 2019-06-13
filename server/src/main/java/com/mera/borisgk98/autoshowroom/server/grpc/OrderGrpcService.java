d49810c0b9003b1330dc03e7cc71c48607ac5100
borisgk98 <borisgk98@ya.ru>
2019-06-13 17:43:01 +0300

    Add grpc to server java

---
+++ server/src/main/java/com/mera/borisgk98/autoshowroom/server/grpc/OrderGrpcService.java
@@ -0,0 +1,6 @@
+package com.mera.borisgk98.autoshowroom.server.grpc;
+
+import com.mera.borisgk98.autoshowroom.grpc.OrderServiceGrpc;
+
+public class OrderGrpcService extends OrderServiceGrpc.OrderServiceImplBase {
+}
